1d7812c1cc101425792db70b997757d1b8102326
Dharmit Shah <shahdharmit@gmail.com>
2017-09-14 13:45:15 +0530

    Remove pid for httpd

--- run-httpd.sh
+++ run-httpd.sh
@@ -3,6 +3,6 @@
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
-# sudo rm -rf /run/httpd/* /tmp/httpd*
+rm -rf /run/httpd/* /tmp/httpd*
 
 exec /usr/sbin/httpd -DFOREGROUND

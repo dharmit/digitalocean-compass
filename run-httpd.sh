ce08835464a951c36795a7cab3516264f71374f7
Dharmit Shah <shahdharmit@gmail.com>
2017-09-14 13:10:27 +0530

    Modify `run-httpd.sh` due to `sudo` error in OpenShift
    OpenShift logs complained:
    
    ```
    sudo: unknown uid 1000060000: who are you?
    sudo: unknown uid 1000060000: who are you?
    ```

--- run-httpd.sh
+++ run-httpd.sh
@@ -3,6 +3,6 @@
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
-sudo rm -rf /run/httpd/* /tmp/httpd*
+# sudo rm -rf /run/httpd/* /tmp/httpd*
 
-sudo bash -c 'exec /usr/sbin/apachectl -DFOREGROUND'
+exec /usr/sbin/httpd -DFOREGROUND
